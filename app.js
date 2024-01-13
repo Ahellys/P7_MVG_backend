c29e5682ed7cb383f4c2322df4785a384c25d276
Yoann Silmon <y.silmon@gmail.com>
2024-01-13 13:58:29 +0100

    Coms

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ require('dotenv').config();
 
 const app = express();
 
-
+//connexion à la BDD
 mongoose.connect(process.env.MONGODB_AUTH)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+//header pour autoriser les requêtes entre différents origines
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
     next();
 });
 
+//conversion des données au format json
 app.use(express.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
